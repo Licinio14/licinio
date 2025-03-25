df3132d9511902c9655c119f309c974047539241
Licinio Sousa <liciniosousa14@gmail.com>
2025-03-25 10:28:05 +0000

    new tp
    trBlho pratico ios

---
+++ inicio_ios/TP_IOS_Licinio_Thiago/TP_IOS_Licinio_Thiago/TP_IOS_Licinio_ThiagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TP_IOS_Licinio_ThiagoApp.swift
+//  TP_IOS_Licinio_Thiago
+//
+//  Created by MultiLab PRT 09 on 25/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TP_IOS_Licinio_ThiagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
